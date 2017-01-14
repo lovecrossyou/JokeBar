3b6e38a4dc64994f5fe8fdaa2f9883f8e0b38ccf
lovecross <lovecrossyoutt@hotmail.com>
2017-01-15 00:23:50 +0800

    python admin学习

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page'),
-    url(r'^edit$',views.edit_article,name='edit_page'),
+    url(r'^edit/(?P<article_id>[0-9]+)$',views.edit_article,name='edit_page'),
     url(r'edit_action$',views.edit_action,name='edit_action')
 ]
